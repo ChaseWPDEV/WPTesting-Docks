8300e8c5df416915b03a010bd7f8dd8188ee6c5d
Chase Gruszewski <chase@cwpdev.com>
2021-12-23 16:48:39 -0800

    -Updated to standard node source -Reorganized backstop for remote volume -Switched backstop to node user

--- buildAll.sh
+++ buildAll.sh
@@ -1,3 +1,3 @@
 docker build -t wpphptests ./phpunit
-docker build -t wpbackstoptests ./backstop
+docker build -t wpbackstoptests --build-arg UID=${UID:-1000} --build-arg GID=${GID:-1000} ./backstop
 docker build -t wpjesttests ./jest

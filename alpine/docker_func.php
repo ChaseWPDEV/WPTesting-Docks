a761b8a20e2f43b389cd0221861b70b6495a488e
Chase Gruszewski <chase@cwpdev.com>
2022-01-27 16:55:14 -0800

    Alpine directory added. Unlike the other containers it is not standalone and will require to be connected with a mysql service to function properly. Intened for use in CI/CD systems, where space may be at a premium

---
+++ alpine/docker_func.php
@@ -0,0 +1,26 @@
+<?php
+if (!function_exists('getenv_docker')) {
+	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
+	function getenv_docker($env, $default) {
+		if ($fileEnv = getenv($env . '_FILE')) {
+			return rtrim(file_get_contents($fileEnv), "\r\n");
+		}
+		else if (($val = getenv($env)) !== false) {
+			return $val;
+		}
+		else {
+			return $default;
+		}
+	}
+}
+
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
+	$_SERVER['HTTPS'] = 'on';
+}
+// (we include this by default because reverse proxying is extremely common in container environments)
+
+if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
+	eval($configExtra);
+}
+
+?>
